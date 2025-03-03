9add0f3234b7f95af6507684408a64c4c2929931
Keusa <ynkig2020@gmail.com>
2025-03-03 21:11:05 +0300

    f: added Knight

---
+++ app/src/main/kotlin/figures/Knight.kt
@@ -0,0 +1,21 @@
+package figures
+import Board.Board
+
+class Knight(override val position: Int, override val colour: Boolean, override val board: Board): Figure {
+    override fun isThereFigure(location: Int): Boolean {
+        return location in board.positions
+    }
+
+    override fun move(newPosition: Int): Boolean {
+        val flag = if(newPosition - position == 15 || position - newPosition == 15) true
+        else if(newPosition - position == 17 || position - newPosition == 17) true
+        else if(newPosition - position == 10 || position - newPosition == 10) true
+        else if(newPosition - position == 6 || position - newPosition == 6) true
+        else return false
+        if(isThereFigure(newPosition) && board.positions[newPosition]?.colour == colour) return false
+        board.positions[newPosition] = this
+        board.positions.remove(position)
+        return true
+    }
+
+}
